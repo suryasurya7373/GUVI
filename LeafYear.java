5d2e97bcce8a55cabf388ea16aa7f66ce4a4aa96
suryasurya7373 <42938108+suryasurya7373@users.noreply.github.com>
2019-06-08 15:43:17 +0530

    Create LeafYear.java

---
+++ LeafYear.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class LeafYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%4==0){
+	System.out.println("yes");
+}else{
+	System.out.println("no");
+}
+	}
+
+}
